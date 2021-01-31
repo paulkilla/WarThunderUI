a39c11bdd8786e3b29e5661c2de3483508c4ef74
Paul <paul.killer@gmail.com>
2021-02-01 09:00:05 +1100

    tidy up logging, and don't try to connect unless the endpoint is up and running

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,10 +34,12 @@ export class AppComponent implements OnInit, OnDestroy {
   enemies: Enemy[];
   pubStatus;
   subStatus;
+  isSiteActive;
 
   constructor(private subService: SubscriptionService, private pubService: PublisherService, private wtService: WarthunderService,
               private ngZone: NgZone) {
     window.restEndpoint = environment.restEndpoint;
+    window.isSiteActive = false;
     this.subService = subService;
     this.pubService = pubService;
     this.wtService = wtService;
@@ -46,6 +48,7 @@ export class AppComponent implements OnInit, OnDestroy {
     this.instruments = new Instruments();
     this.enemies = [];
     this.teamInstruments = [];
+    this.isSiteActive = false;
   }
 
   ngOnInit(): void {
@@ -59,7 +62,7 @@ export class AppComponent implements OnInit, OnDestroy {
         rawData => {
             const data = JSON.parse(rawData);
             const message_type = data.message_type;
-            if (message_type == null) {
+            if (message_type == null && data.message !== 'Squad joined') {
               console.log('Data has no message type: ' + JSON.stringify(data));
             } else {
               if (message_type === 'squadmate') {
@@ -82,10 +85,26 @@ export class AppComponent implements OnInit, OnDestroy {
           err => console.log( 'pub err'),
           () =>  console.log( 'The observable pub stream is complete')
         );
+    this.monitorEndPointHealth();
     this.registerWithSquad();
     this.monitorInstruments();
     this.monitorGameChat();
     this.monitorGameLog();
+  }
+
+  monitorEndPointHealth(): void {
+    interval(3000).subscribe((x: any) => {
+      isSiteOnline(found => {
+        if (found) {
+          window.isSiteActive = true;
+          this.isSiteActive = true;
+        }
+        else {
+          window.isSiteActive = false;
+          this.isSiteActive = false;
+        }
+      });
+    });
   }
 
   registerWithSquad(): void {
@@ -131,7 +150,7 @@ export class AppComponent implements OnInit, OnDestroy {
     }
   }
 
-  handleEnemyMessage(data): void {c
+  handleEnemyMessage(data): void {
     if (existsInArray(this.enemies, 'name', data.player)) {
       this.enemies.forEach((enemy, index) => {
         if (enemy.name === data.player) {
@@ -163,159 +182,155 @@ export class AppComponent implements OnInit, OnDestroy {
     $('#water-trend-line').sparkline(waterSLArray);
 
     interval(2000).subscribe((x: any) => {
-      this.wtService.getState().subscribe(state => {
-        // Assign variables in state to this.instruments
-        for (const prop in state) {
-          if (Object.prototype.hasOwnProperty.call(state, prop)) {
-            this.instruments[prop] = state[prop];
-            // Only update spark line when we are in a game, else reset them to 0
-            if (this.inGame) {
-              if (prop === 'ias') {
-                iasSLArray.push(state.ias);
-                $('#ias-trend-line').sparkline(iasSLArray);
-              } else if (prop === 'altitude') {
-                altitudeSLArray.push(state.altitude);
-                $('#altitude-trend-line').sparkline(altitudeSLArray);
-              } else if (prop === 'throttle') {
-                throttleSLArray.push(state.throttle);
-                $('#throttle-trend-line').sparkline(throttleSLArray);
-              } else if (prop === 'verticalSpeed') {
-                climbRateSLArray.push(state.verticleSpeed);
-                $('#climb-rate-trend-line').sparkline(climbRateSLArray);
-              } else if (prop === 'climbAngle') {
-                climbAngleSLArray.push(state.climbAngle);
-                $('#climb-angle-trend-line').sparkline(climbAngleSLArray);
-              } else if (prop === 'oilTemp') {
-                oilSLArray.push(state.oilTemp);
-                $('#oil-trend-line').sparkline(oilSLArray);
-              } else if (prop === 'waterTemp') {
-                waterSLArray.push(state.waterTemp);
-                $('#water-trend-line').sparkline(waterSLArray);
+      if (this.isSiteActive) {
+        this.wtService.getState().subscribe(state => {
+          // Assign variables in state to this.instruments
+          for (const prop in state) {
+            if (Object.prototype.hasOwnProperty.call(state, prop)) {
+              this.instruments[prop] = state[prop];
+              // Only update spark line when we are in a game, else reset them to 0
+              if (this.inGame) {
+                if (prop === 'ias') {
+                  iasSLArray.push(state.ias);
+                  $('#ias-trend-line').sparkline(iasSLArray);
+                } else if (prop === 'altitude') {
+                  altitudeSLArray.push(state.altitude);
+                  $('#altitude-trend-line').sparkline(altitudeSLArray);
+                } else if (prop === 'throttle') {
+                  throttleSLArray.push(state.throttle);
+                  $('#throttle-trend-line').sparkline(throttleSLArray);
+                } else if (prop === 'verticalSpeed') {
+                  climbRateSLArray.push(state.verticleSpeed);
+                  $('#climb-rate-trend-line').sparkline(climbRateSLArray);
+                } else if (prop === 'climbAngle') {
+                  climbAngleSLArray.push(state.climbAngle);
+                  $('#climb-angle-trend-line').sparkline(climbAngleSLArray);
+                } else if (prop === 'oilTemp') {
+                  oilSLArray.push(state.oilTemp);
+                  $('#oil-trend-line').sparkline(oilSLArray);
+                } else if (prop === 'waterTemp') {
+                  waterSLArray.push(state.waterTemp);
+                  $('#water-trend-line').sparkline(waterSLArray);
+                }
+              } else {
+                iasSLArray = [0];
+                altitudeSLArray = [0];
+                throttleSLArray = [0];
+                climbRateSLArray = [0];
+                climbAngleSLArray = [0];
+                oilSLArray = [0];
+                waterSLArray = [0];
               }
-            } else {
-              iasSLArray = [0];
-              altitudeSLArray = [0];
-              throttleSLArray = [0];
-              climbRateSLArray = [0];
-              climbAngleSLArray = [0];
-              oilSLArray = [0];
-              waterSLArray = [0];
             }
           }
-        }
-      });
+        });
+      }
     });
     interval(2000).subscribe((x: any) => {
-      this.wtService.getIndicators().subscribe(indicators => {
-        if (indicators == null || indicators.valid == null || !indicators.valid) {
-          this.gameChat = [];
-          this.enemies = [];
-          this.inGame = false;
-          this.teamInstruments.forEach(instrument => {
-            instrument.killed = false;
-          });
-        } else {
-          this.inGame = true;
-        }
-        // Assign variables in indicators to this.instruments
-        for (const prop in indicators) {
-          if (Object.prototype.hasOwnProperty.call(indicators, prop)) {
-            this.instruments[prop] = indicators[prop];
-          }
-        }
+      if (this.isSiteActive) {
+        this.wtService.getIndicators().subscribe(indicators => {
+          if (this.isSiteActive) {
+            if (indicators == null || indicators.valid == null || !indicators.valid) {
+              this.gameChat = [];
+              this.enemies = [];
+              this.inGame = false;
+              this.teamInstruments.forEach(instrument => {
+                instrument.killed = false;
+              });
+            } else {
+              this.inGame = true;
+            }
+            // Assign variables in indicators to this.instruments
+            for (const prop in indicators) {
+              if (Object.prototype.hasOwnProperty.call(indicators, prop)) {
+                this.instruments[prop] = indicators[prop];
+              }
+            }
 
-        if (this.inGame) {
-          this.instruments.playerName = localStorage.getItem('playerName');
-          this.pubStatus = this.pubService.sendMessage(JSON.stringify({
-            message_type: 'squadmate',
-            player: localStorage.getItem('playerName'),
-            data: this.instruments
-          }));
-        }
-      });
+            if (this.inGame) {
+              this.instruments.playerName = localStorage.getItem('playerName');
+              this.pubStatus = this.pubService.sendMessage(JSON.stringify({
+                message_type: 'squadmate',
+                player: localStorage.getItem('playerName'),
+                data: this.instruments
+              }));
+            }
+          }
+        });
+      }
     });
   }
 
   monitorGameChat(): void {
     interval(2000).subscribe((x: any) => {
-      let latestId = 0;
-      this.gameChat.forEach(item => {
-        latestId = item.id;
-      });
-      this.wtService.getGameChat(latestId).subscribe(gameChat => gameChat.forEach((item: any) => {
-        this.gameChat.push(item);
-        updateScroll();
-        const myRegexResult = item.msg.match('.*( )(.*)(\\(.*\\))!.*$');
-        const otherRegexResult = item.msg.match('(.*)\\s+(.*)(\\(.*\\))![<]\\bcolor(.*)[>]\\s\\[(.*)\\][<][/]\\bcolor\\b[>]$');
-        if ( myRegexResult != null || otherRegexResult != null ) {
-          let lastLocation = 'Unknown';
-          const lastSeen = new Date().getTime();
-          let playerName = '';
-          let playerPlane = '';
-          if (myRegexResult != null) {
-            playerName = myRegexResult[2];
-            playerPlane = myRegexResult[3];
+      if (this.isSiteActive) {
+        let latestId = 0;
+        this.gameChat.forEach(item => {
+          latestId = item.id;
+        });
+        this.wtService.getGameChat(latestId).subscribe(gameChat => gameChat.forEach((item: any) => {
+          this.gameChat.push(item);
+          updateScroll();
+          const myRegexResult = item.msg.match('.*( )(.*)(\\(.*\\))!.*$');
+          const otherRegexResult = item.msg.match('(.*)\\s+(.*)(\\(.*\\))![<]\\bcolor(.*)[>]\\s\\[(.*)\\][<][/]\\bcolor\\b[>]$');
+          if ( myRegexResult != null || otherRegexResult != null ) {
+            let lastLocation = 'Unknown';
+            const lastSeen = new Date().getTime();
+            let playerName = '';
+            let playerPlane = '';
+            if (myRegexResult != null) {
+              playerName = myRegexResult[2];
+              playerPlane = myRegexResult[3];
+            }
+            if (otherRegexResult != null) {
+              playerName = otherRegexResult[2];
+              playerPlane = otherRegexResult[3];
+              lastLocation = otherRegexResult[5];
+            }
+            this.pubStatus = this.pubService.sendMessage(JSON.stringify({
+              message_type: 'enemy',
+              player: playerName,
+              data: {altitude: '', lastSeen, name: playerName, plane: playerPlane, killed: false,
+                location: lastLocation}
+            }));
           }
-          if (otherRegexResult != null) {
-            playerName = otherRegexResult[2];
-            playerPlane = otherRegexResult[3];
-            lastLocation = otherRegexResult[5];
-          }
-          this.pubStatus = this.pubService.sendMessage(JSON.stringify({
-            message_type: 'enemy',
-            player: playerName,
-            data: {altitude: '', lastSeen, name: playerName, plane: playerPlane, killed: false,
-              location: lastLocation}
-          }));
-        }
-      }));
+        }));
+      }
     });
   }
 
   monitorGameLog(): void {
     interval(2000).subscribe((x: any) => {
-      const latestEvtId = 0;
-      let latestDmgId = 0; // Not sure what this one does atm
-      this.hudMessages.forEach(item => {
-        latestDmgId = item.id;
-      });
-      this.wtService.getHudMessages(latestEvtId, latestDmgId).subscribe(hudMessages => hudMessages.forEach((item: any) => {
-        this.hudMessages.push(item);
-        // Do stuff here with the item and set enemies as dead etc.
-        let regexResult = item.msg.match('(.*)(\\(.*\\))[\\s](.*)[\\s](.*)\\s(\\(.*\\)).*$');
-        if ( regexResult != null ) {
-          // Do stuff here when we match on the regex to pull down 'shot down'
-          const action = regexResult[3];
-          if (action != null) {
-            if ( action.startsWith('shot down')) {
-              const targetPlayerName = regexResult[4];
-              if (targetPlayerName === this.instruments.playerName) {
-                this.instruments.killed = true;
-              }
-              this.enemies.forEach((enemy: any) => {
-                if (enemy.name === targetPlayerName) {
-                  enemy.killed = true;
-                }
-              });
-            }
-          }
-        } else {
-          // Check for other regex.
-          // Check for crashes.. hah what a noob!
-          regexResult = item.msg.match('[\\s?](.*) (\\(.*\\))(.*)[ has crashed.]$');
+      if (this.isSiteActive) {
+        const latestEvtId = 0;
+        let latestDmgId = 0; // Not sure what this one does atm
+        this.hudMessages.forEach(item => {
+          latestDmgId = item.id;
+        });
+        this.wtService.getHudMessages(latestEvtId, latestDmgId).subscribe(hudMessages => hudMessages.forEach((item: any) => {
+          this.hudMessages.push(item);
+          // Do stuff here with the item and set enemies as dead etc.
+          let regexResult = item.msg.match('(.*)(\\(.*\\))[\\s](.*)[\\s](.*)\\s(\\(.*\\)).*$');
           if ( regexResult != null ) {
-            const targetPlayerName = regexResult[1];
-            if (targetPlayerName === this.instruments.playerName) {
-              this.instruments.killed = true;
-            }
-            this.enemies.forEach((enemy: any) => {
-              if (enemy.name === targetPlayerName) {
-                enemy.killed = true;
+            // Do stuff here when we match on the regex to pull down 'shot down'
+            const action = regexResult[3];
+            if (action != null) {
+              if ( action.startsWith('shot down')) {
+                const targetPlayerName = regexResult[4];
+                if (targetPlayerName === this.instruments.playerName) {
+                  this.instruments.killed = true;
+                }
+                this.enemies.forEach((enemy: any) => {
+                  if (enemy.name === targetPlayerName) {
+                    enemy.killed = true;
+                  }
+                });
               }
-            });
+            }
           } else {
-            // check for aaa kill
-            regexResult = item.msg.match('\\bAAA shot down\\b\\s(.*)\\s(\\(.*\\))$');
+            // Check for other regex.
+            // Check for crashes.. hah what a noob!
+            regexResult = item.msg.match('[\\s?](.*) (\\(.*\\))(.*)[ has crashed.]$');
             if ( regexResult != null ) {
               const targetPlayerName = regexResult[1];
               if (targetPlayerName === this.instruments.playerName) {
@@ -326,19 +341,33 @@ export class AppComponent implements OnInit, OnDestroy {
                   enemy.killed = true;
                 }
               });
+            } else {
+              // check for aaa kill
+              regexResult = item.msg.match('\\bAAA shot down\\b\\s(.*)\\s(\\(.*\\))$');
+              if ( regexResult != null ) {
+                const targetPlayerName = regexResult[1];
+                if (targetPlayerName === this.instruments.playerName) {
+                  this.instruments.killed = true;
+                }
+                this.enemies.forEach((enemy: any) => {
+                  if (enemy.name === targetPlayerName) {
+                    enemy.killed = true;
+                  }
+                });
+              }
+            }
+            // Match for Awards
+            regexResult = item.msg.match('(.*)\\s(\\(.*\\))\\s\\bhas achieved\\b\\s(.*)$');
+            if ( regexResult != null ) {
+              const player = regexResult[1];
+              const award = regexResult[3];
+              if (existsInArray(this.teamInstruments, 'playerName', player) || player === localStorage.getItem('playerName')) {
+                showNotification('top', 'left', player + ' got the award ' + award, 'warning', 3000, false);
+              }
             }
           }
-          // Match for Awards
-          regexResult = item.msg.match('(.*)\\s(\\(.*\\))\\s\\bhas achieved\\b\\s(.*)$');
-          if ( regexResult != null ) {
-            const player = regexResult[1];
-            const award = regexResult[3];
-            if (existsInArray(this.teamInstruments, 'playerName', player) || player === localStorage.getItem('playerName')) {
-              showNotification('top', 'left', player + ' got the award ' + award, 'warning', 3000, false);
-            }
-          }
-        }
-      }));
+        }));
+      }
     });
   }
 
