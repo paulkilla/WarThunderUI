15c9d61829f0f55228158c450445022f6f66e1d4
Paul <paul.killer@gmail.com>
2021-02-17 23:28:34 +1100

    Migrate to use node-js server, test it out

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   restEndpoint: 'http://squad.wolfpack.ws/v1',
+  wsWpEndpoint: 'ws://squad.wolfpack.ws/',
   wsSubEndpoint: 'ws://squad.wolfpack.ws/v1/sub',
   wsPubEndpoint: 'ws://squad.wolfpack.ws/v1/pub'
 };
